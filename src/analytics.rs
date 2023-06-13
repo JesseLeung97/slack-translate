9bfafab17c1bf85ddb897678bdf3f3691ac54484
Jesse Leung <jesse@steins.co.jp>
2023-06-14 00:11:38 +0900

    [Update] Work on analytics

--- src/analytics.rs
+++ src/analytics.rs
@@ -1,11 +1,24 @@
+use std::error::Error;
 use redis::aio::ConnectionManager;
 use crate::models::parse_user;
-use crate::cache::increment_user_count;
+use crate::cache::{increment_user_count, increment_language_count, increment_total_count};
+use crate::models::Language;
 
-pub fn update_analytics(mut connection_manager: ConnectionManager, user_id: &str, message: &str, translation: &str) -> Result<Ok, Box<dyn Error>> {
+pub async fn update_analytics(
+    connection_manager: ConnectionManager, 
+    user_id: &str,
+    language: Language, 
+    message: &str, 
+    translation: &str
+) -> Result<(), Box<dyn Error>> {
     let user = parse_user(user_id).unwrap();
 
-    increment_user_count(user_id)?;
+    let _ = increment_user_count(user_id, connection_manager.clone()).await?;
+    let _ = increment_language_count(language, connection_manager.clone()).await?;
+    let _ = increment_total_count(connection_manager.clone()).await?;
+
+
     
+
     Ok(())
 }
--- src/cache.rs
+++ src/cache.rs
@@ -1,4 +1,5 @@
 use redis::{aio::ConnectionManager, cmd, Client, FromRedisValue, RedisError, RedisResult};
+use crate::models::Language;
 
 pub async fn get_redis_connection_manager() -> RedisResult<ConnectionManager> {
     let client = Client::open("redis://localhost:6380")?;
@@ -12,7 +13,7 @@ pub async fn check_cache(
     mut connection_manager: ConnectionManager,
 ) -> Option<String> {
     let mut command = cmd("GET");
-    command.arg(req_text);
+    let command = command.arg(req_text);
 
     let redis_value = match connection_manager.send_packed_command(&command).await {
         Err(_) => return None,
@@ -29,12 +30,37 @@ pub async fn check_cache(
 pub async fn increment_user_count(
     user_id: &str,
     mut connection_manager: ConnectionManager
-) -> Result<(), RedisError> {
+) -> Result<usize, RedisError> {
     let mut command = cmd("INCR");
-    command.arg(user_id);
+    let command = command.arg(user_id);
 
-    let redis_result = connection_manager.send_packed_command(&command).await?;
-    println!("{:?}", redis_result);
+    let redis_value: redis::Value = connection_manager.send_packed_command(&command).await?;
+    let user_translation_count: usize = FromRedisValue::from_redis_value(&redis_value)?;
 
-    Ok(())
+    Ok(user_translation_count)
+}
+
+pub async fn increment_language_count(
+    language: Language,
+    mut connection_manager: ConnectionManager
+) -> Result<usize, RedisError> {
+    let mut command = cmd("INCR");
+    let command = command.arg(language.to_string());
+
+    let redis_value = connection_manager.send_packed_command(&command).await?;
+    let language_translation_count: usize = FromRedisValue::from_redis_value(&redis_value)?;
+
+    Ok(language_translation_count)
+}
+
+pub async fn increment_total_count(
+    mut connection_manager: ConnectionManager
+) -> Result<usize, RedisError> {
+    let mut command = cmd("INCR");
+    let command = command.arg("TOTAL");
+
+    let redis_value = connection_manager.send_packed_command(&command).await?;
+    let total_count: usize = FromRedisValue::from_redis_value(&redis_value)?;
+
+    Ok(total_count)
 }
