aaa7c04f4647fccde4b55cd1269bd0d1f58f2a78
Jesse Leung <jessetleung@aol.com>
2023-06-27 19:57:12 +0900

    [Update] Work on analytics

--- src/analytics.rs
+++ src/analytics.rs
@@ -1,18 +1,11 @@
 use std::error::Error;
 use sqlite::ConnectionWithFullMutex;
 use crate::models::Language;
-use crate::database::get_users;
+use crate::database::{ get_translation_log, get_users };
 
 pub fn get_analytics(database_connection: &ConnectionWithFullMutex) -> Result<(), Box<dyn Error>> {
     let users = get_users(&database_connection)?;
+    let translation_log = get_translation_log(&database_connection);
 
-    // let user = parse_user(user_id).unwrap();
-
-    // let _ = increment_user_count(user_id, connection_manager.clone()).await?;
-    // let _ = increment_language_count(source_language, connection_manager.clone()).await?;
-    // let _ = increment_total_count(connection_manager.clone()).await?;
-
-    // let _ = append_to_translation_log(&user, source_language, message, translation)?;
-    
     Ok(())
 }
