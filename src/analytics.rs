ceab2ab4eeb0d0cab6e0f9a80bc26daf68f2e5ea
Jesse Leung <jesse@steins.co.jp>
2023-07-07 01:39:40 +0900

    Css changes

--- src/analytics.rs
+++ src/analytics.rs
@@ -14,7 +14,8 @@ pub fn get_analytics(database_connection: &ConnectionWithFullMutex) -> Result<An
 
     for translation in &translation_log {
         count_by_language.entry(translation.language.to_string()).and_modify(|count| *count += 1).or_insert(1);
-        count_by_date.entry(translation.created.to_owned()).and_modify(|count| *count += 1).or_insert(1);
+        let translated_date = translation.created.to_owned().as_str()[..=10].to_string();
+        count_by_date.entry(translated_date).and_modify(|count| *count += 1).or_insert(1);
         count_by_team_member.entry(translation.user_name.to_owned()).and_modify(|count| *count += 1).or_insert(1);
         total_count += 1;
     }
--- src/filewriter.rs
+++ src/filewriter.rs
@@ -6,7 +6,7 @@ use aho_corasick::AhoCorasick;
 use serde::{Serialize, Deserialize};
 use sqlite::ConnectionWithFullMutex;
 use std::fmt::Display;
-use crate::analytics::get_analytics;
+use crate::analytics::{get_analytics, self};
 use crate::models::{TranslationLog, Language};
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
@@ -41,6 +41,7 @@ pub fn populate_html(filename: &str, database_connection: &ConnectionWithFullMut
     if filename.len() > 0 {
         filename.remove(0);
     }
+    println!("{}",filename);
 
     let file_path = format!("src/html/{}.html", &filename);
     let generated_file_path = format!("generated/{}.html", &filename);
@@ -50,7 +51,36 @@ pub fn populate_html(filename: &str, database_connection: &ConnectionWithFullMut
     let file_contents = read_to_string(&file_path)?;
 
     let ac_input = AhoCorasick::new(vec!["{Value}", "{TranslationLog}"]).unwrap();
-    let ac_replace_strings = vec!["I'm replaced!", "Number two!"];
+
+    let total_en = analytics.count_by_language.get::<String>(&Language::EN.to_string());
+    let total_en: usize= if let Some(total) = total_en {
+        total.to_owned()
+    } else {
+        0usize
+    };
+    let total_ja = analytics.count_by_language.get::<String>(&Language::JA.to_string());
+    let total_ja: usize= if let Some::<&usize>(total) = total_ja {
+        total.to_owned()
+    } else {
+        0usize
+    };
+    let total_count = analytics.total_count.to_string();
+
+    let percent_en = ((total_en as f64 / analytics.total_count as f64) * 100f64).round() as usize;
+    let percent_ja = ((total_ja as f64 / analytics.total_count as f64) * 100f64).round() as usize;
+ 
+    let total_en_string = total_en.to_string();
+    let total_ja_string = total_ja.to_string();
+    let percent_en_string = percent_en.to_string();
+    let percent_ja_string = percent_ja.to_string();
+
+    let ac_replace_strings = vec![
+        total_count.as_str(), 
+        total_en_string.as_str(), 
+        total_ja_string.as_str(),
+        percent_en_string.as_str(), 
+        percent_ja_string.as_str(), 
+    ];
     
     let mut iter = 0;
     let mut replace_result = String::new();
@@ -88,6 +118,7 @@ fn loop_translation_log(translation_logs: Vec<TranslationLog>) -> String {
             Language::EN => Language::JA.to_string(),
             Language::JA => Language::EN.to_string()
         };
+        let translated_date = translation_log.created.to_owned().as_str()[..=10].to_string();
         let html_string = format!(
         "<div class=\"tl-outer\">
             <div class=\"tl-label-outer\">
@@ -107,7 +138,7 @@ fn loop_translation_log(translation_logs: Vec<TranslationLog>) -> String {
             </div>
         </div>
         ",
-            translation_log.created,
+            translated_date,
             translation_log.user_name,
             translation_log.language.to_string(),
             translation_res_language,
